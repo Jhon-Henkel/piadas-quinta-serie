b0bf9e405101ba7709c1d6799c4384304a95c437
Jhonatan Henkel <jhowrf@gmail.com>
2023-09-07 14:38:49 -0300

    Desenvolvido página de piadas

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     
     <router-view class="mt-5 mb-5 ms-5 me-5"/>
     <div class="footer">
-        Desenvolvido com &#129505; por
+        Desenvolvido por
         <a class="footer-link" target="_blank" href="https://jhon.dev.br">
             <strong>
                 Jhonatan Henkel
--- src/views/JokesView.vue
+++ src/views/JokesView.vue
@@ -1,6 +1,50 @@
-<script setup>
+<script>
+
+export default {
+    name: 'JokesView',
+    data() {
+        return {
+            joke: ''
+        }
+    },
+    methods: {
+        nextJoke() {
+            const jokes = [
+                'Seu cachorro nabunda cai no mar, vc deixa nabunda ou leva nabunda?',
+                'Voce pinta como eu pinto?',
+                'Tifu, tifu, tifu, quantos tifu deu?',
+                'Jacaré em terrenos alagados anda,mais no secu anda?',
+                'Tenho 1 Kg de arroz, seu cuzinho, ele é meu?',
+                'Já viu o palhaço que se esconde de viado?',
+                'Vou fazer um churrasco, 30 kg de carne, sera q da pra 20 comer?',
+                'E a onça? aquela pintada que eu te dei',
+                'Texto bom é o pequeno ou você gosta do grandão?',
+                'Vamos apostar? Mil meu com mil seu',
+                'Se eu passar roupa, você leva o ferro?',
+                'Qual é o aumentativo de dacueba?',
+                ''
+
+            ];
+            this.joke = jokes[Math.floor(Math.random() * jokes.length)];
+        }
+    },
+    mounted() {
+        this.nextJoke();
+    }
+}
 </script>
 
 <template>
-    <h1>Piadas</h1>    
+    <div>
+        <div>
+            <h1>Bora rir um pouco...</h1>
+            <p class="mt-4 mb-5 joke">
+                {{ joke }}
+            </p>
+        </div>
+        <div>
+            <button class="btn btn-success btn-full mb-2" v-on:click="nextJoke">Próxima</button>
+            <RouterLink to="/" class="btn btn-success btn-full">Voltar</RouterLink>
+        </div>
+    </div>
 </template>
