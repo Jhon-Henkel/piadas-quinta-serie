94b73557c81f0873c9564a345928589db00b6471
Jhonatan Henkel <jhowrf@gmail.com>
2023-09-07 13:57:16 -0300

    Desenvolvido tela principal
    Desenvolvido tela principal e já deixado o css padronizado.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
 <template>
-  <router-view />
+  <div class="glass">
+    
+    <router-view class="mt-5 mb-5 ms-5 me-5"/>
+    <div class="footer">
+        Desenvolvido com &#129505; por
+        <a class="footer-link" target="_blank" href="https://jhon.dev.br">
+            <strong>
+                Jhonatan Henkel
+            </strong>
+        </a>
+      </div>
+  </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,14 @@
-<script setup>
-</script>
-
 <template>
-  <RouterLink to="/piadas">Vamos Lá</RouterLink>
+  <div>
+    <h1>Bem vindo ao Piadas Quinta Série</h1>
+    <p class="mt-4">
+      Um site para você se divertir com as piadas fizeram parte de nossa tão amada quinta série.
+    </p>
+    <p>
+      Mas antes, um aviso...
+      As piadas contidas neste site são de cunho humorístico e não devem ser
+      levadas a sério.
+    </p>
+    <RouterLink to="/piadas" class="btn btn-success btn-full mt-4">Vamos rir!</RouterLink>
+  </div>
 </template>
